4110ec230f0753651be5ac54d4a52a58d80063cc
ckyue <yuechuankai@gmail.com>
2016-02-07 13:59:31 -0500

    continue btn

--- js/main.js
+++ js/main.js
@@ -72,3 +72,10 @@ $("#tocontacts").click(function () {
     $("html,body").animate({scrollTop: pos}, 1000);
     return false;
 });
+
+$("#continue").click(function () {
+    var href = $(this).attr("href");
+    var pos = $(href).offset().top;
+    $("html,body").animate({scrollTop: pos}, 1000);
+    return false;
+});
